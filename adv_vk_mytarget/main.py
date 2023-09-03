a536d0e7406d72c06d49a9e9ecca544101553305
artem <sashheka@gmail.com>
2023-09-03 14:50:00 +0300

    refactored methods for both mytarget and vk

--- adv_vk_mytarget/main.py
+++ adv_vk_mytarget/main.py
@@ -1,22 +1,23 @@
-from methods import get_campaigns, get_ads, get_campaigns_statistics, get_ads_statistics, load_to_database
+from methods import get_campaigns, get_ads, get_campaigns_statistics, get_ads_statistics, load_to_database, convert_to_dataframe
 import pandas as pd
 import requests
 import datetime
 import json
 
 
-client_id = 17778548
-client_name = 'adv_cabandos_vk'
+client_name = 'adv_hr_belagroprombank_mytarget'
 with open('../resources/adv_tokens.json') as json_file:
     data = json.load(json_file)
-access_token = data[client_name]['access_token']
+
+for i in data:
+    if i['client_name'] == client_name:
+        access_token = i['access_token']
 
 date_to = datetime.date.today()
 date_from = str(datetime.date(date_to.year, date_to.month - 2, 1))
 
 
-campaigns = get_campaigns(client_id=client_id,
-                          client_name=client_name,
+campaigns = get_campaigns(client_name=client_name,
                           access_token=access_token)
 
 campaigns_df = pd.DataFrame(campaigns['items'])
@@ -28,17 +29,16 @@ campaigns_statistics = get_campaigns_statistics(date_from=date_from,
                                                 campaigns_ids=campaigns_ids,
                                                 client_name=client_name,
                                                 access_token=access_token)
-table_name = f'{client_name}_campaigns_statistics'
 
-df = pd.json_normalize(campaigns_statistics['items'], 'rows', 'id')
-df = pd.merge(df, campaigns_df[['id', 'name']], on='id', how='left')
-df.to_csv(f'assets/{table_name}.csv', index=False)
-df = pd.read_csv(f'assets/{table_name}.csv')
-# # load_to_database(df, client_name)
+df = convert_to_dataframe(statistics=campaigns_statistics,
+                          reference_df=campaigns_df,
+                          client_name=client_name,
+                          category='campaigns')
+
+# load_to_database(df, table_name)
 
 
-ads = get_ads(client_id=client_id,
-              client_name=client_name,
+ads = get_ads(client_name=client_name,
               access_token=access_token)
 
 ads_df = pd.DataFrame(ads['items'], columns=['id', 'name', ])
@@ -50,10 +50,10 @@ ads_statistics = get_ads_statistics(date_from=date_from,
                                     ads_ids=ads_ids,
                                     client_name=client_name,
                                     access_token=access_token)
-table_name = f'{client_name}_ads_statistics'
 
-df = pd.json_normalize(ads_statistics['items'], 'rows', 'id')
-df = pd.merge(df, ads_df[['id', 'name']], on='id', how='left')
-df.to_csv(f'assets/{table_name}.csv', index=False)
-df = pd.read_csv(f'assets/{table_name}.csv')
-# load_to_database(df, client_name)
+df = convert_to_dataframe(statistics=ads_statistics,
+                          reference_df=ads_df,
+                          client_name=client_name,
+                          category='ads')
+
+# load_to_database(df, table_name)
--- adv_vk_mytarget/methods.py
+++ adv_vk_mytarget/methods.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import sqlalchemy
 import requests
 import json
@@ -7,38 +8,44 @@ with open('../resources/sql_engine.txt', 'r') as file:
     sql_engine = file.read()
 
 
-def get_campaigns(client_id, client_name, access_token):
-    url = 'https://ads.vk.com/api/v2/ad_plans.json'
+def get_campaigns(client_name, access_token):
+    if 'mytarget' in client_name:
+        url = 'https://ads.vk.com/api/v2/campaigns.json'
+    else:
+        url = 'https://ads.vk.com/api/v2/ad_plans.json'
     params = {
-        'id': client_id,
-        'metrics': 'all'
+        'metrics': 'all',
+        'limit': 250,
     }
     headers = {
         'Authorization': f'Bearer {access_token}'
     }
     response = requests.get(url, params=params, headers=headers).json()
-    # with open(f'assets/{client_name}_campaigns.json', 'w') as f:
-    #     json.dump(response, f, indent=3)
+    with open(f'assets/{client_name}_campaigns.json', 'w') as f:
+        json.dump(response, f, indent=3)
     return response
 
 
-def get_ads(client_id, client_name, access_token):
+def get_ads(client_name, access_token):
     url = 'https://ads.vk.com/api/v2/banners.json'
     params = {
-        'id': client_id,
-        'fields': 'id, name'
+        'fields': 'id, name',
+        'limit': 250,
     }
     headers = {
         'Authorization': f'Bearer {access_token}'
     }
     response = requests.get(url, params=params, headers=headers).json()
-    # with open(f'assets/{client_name}_ads.json', 'w') as f:
-    #     json.dump(response, f, indent=3)
+    with open(f'assets/{client_name}_ads.json', 'w') as f:
+        json.dump(response, f, indent=3)
     return response
 
 
 def get_campaigns_statistics(date_from, date_to, campaigns_ids, client_name, access_token):
-    url = 'https://ads.vk.com/api/v2/statistics/ad_plans/day.json'
+    if 'mytarget' in client_name:
+        url = 'https://ads.vk.com/api/v2/statistics/campaigns/day.json'
+    else:
+        url = 'https://ads.vk.com/api/v2/statistics/ad_plans/day.json'
     params = {
         'date_from': date_from,
         'date_to': str(date_to),
@@ -76,3 +83,13 @@ def load_to_database(df, table_name):
         df.head(0).to_sql(table_name, engine, if_exists='replace', index=False)
     df.to_sql(table_name, engine, if_exists='replace', index=False)
     print(f'{table_name} loaded to database')
+
+
+def convert_to_dataframe(statistics, reference_df, client_name, category):
+    table_name = f'{client_name}_{category}_statistics'
+    df = pd.json_normalize(statistics['items'], 'rows', 'id')
+    df = pd.merge(df, reference_df[['id', 'name']], on='id', how='left')
+    df.to_csv(f'assets/{table_name}.csv', index=False)
+    df = pd.read_csv(f'assets/{table_name}.csv')
+    df.columns = df.columns.str.replace('.', '_', regex=True)
+    return df
